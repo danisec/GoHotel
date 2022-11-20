33719102c5c9ee41ed8a92012eab0d7388006ce8
danisec <daniaprilyanto35@gmail.com>
2022-11-20 19:19:25 +0700

    add authReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+import authReducer from '../features/auth/authSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
 
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
 });
