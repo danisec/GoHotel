8163138fa89f5a42b03235cd2a0e2e7d5a2e7435
danisec <daniaprilyanto35@gmail.com>
2022-11-26 15:19:54 +0700

    udpdate login alert notification

--- Screens/login/Login.js
+++ Screens/login/Login.js
@@ -3,6 +3,7 @@ import {useNavigation} from '@react-navigation/native';
 import {useDispatch, useSelector} from 'react-redux';
 import {authUser} from '../../src/features/auth/authSlice';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {AlertNotificationRoot} from 'react-native-alert-notification';
 import {
   SafeAreaView,
   View,
@@ -43,53 +44,55 @@ export default function Login() {
   }, [user, navigate]);
 
   return (
-    <SafeAreaView className="p-2 h-full pt-28 bg-white">
-      <View className="flex flex-row justify-center">
-        <Text className="text-lg font-semibold">Sign In</Text>
-      </View>
+    <AlertNotificationRoot>
+      <SafeAreaView className="p-2 h-full pt-28 bg-white">
+        <View className="flex flex-row justify-center">
+          <Text className="text-lg font-semibold">Sign In</Text>
+        </View>
 
-      <View className="flex flex-row justify-center">
-        <Text className="text-sm text-gray-500">
-          Sign in to your account to continue
-        </Text>
-      </View>
-
-      <View className="flex flex-row justify-center mt-4">
-        <TextInput
-          className="border border-gray-100 rounded-md p-2 w-3/4"
-          placeholder="Email"
-          onChangeText={emailHandler}
-        />
-      </View>
-
-      <View className="flex flex-row justify-center mt-4">
-        <TextInput
-          className="border border-gray-100 rounded-md p-2 w-3/4"
-          placeholder="Password"
-          secureTextEntry={secureText ? false : true}
-          onChangeText={passwordHandler}
-        />
-
-        <TouchableHighlight className="absolute mt-3 right-14">
-          <Icon
-            name={secureText === false ? 'visibility' : 'visibility-off'}
-            size={20}
-            color="#000"
-            onPress={secureTextHandler}
-          />
-        </TouchableHighlight>
-      </View>
-
-      <View className="flex flex-row justify-center mt-4">
-        <TouchableHighlight
-          className="bg-blue-500 rounded-full py-3 mt-2 w-3/4"
-          underlayColor={'#2192FF'}
-          onPress={loginHandler}>
-          <Text className="text-white text-center">
-            {status === 'loading' ? 'Loading...' : 'Sign In'}
+        <View className="flex flex-row justify-center">
+          <Text className="text-sm text-gray-500">
+            Sign in to your account to continue
           </Text>
-        </TouchableHighlight>
-      </View>
-    </SafeAreaView>
+        </View>
+
+        <View className="flex flex-row justify-center mt-4">
+          <TextInput
+            className="border border-gray-100 rounded-md p-2 w-3/4"
+            placeholder="Email"
+            onChangeText={emailHandler}
+          />
+        </View>
+
+        <View className="flex flex-row justify-center mt-4">
+          <TextInput
+            className="border border-gray-100 rounded-md p-2 w-3/4"
+            placeholder="Password"
+            secureTextEntry={secureText ? false : true}
+            onChangeText={passwordHandler}
+          />
+
+          <TouchableHighlight className="absolute mt-3 right-14">
+            <Icon
+              name={secureText === false ? 'visibility' : 'visibility-off'}
+              size={20}
+              color="#000"
+              onPress={secureTextHandler}
+            />
+          </TouchableHighlight>
+        </View>
+
+        <View className="flex flex-row justify-center mt-4">
+          <TouchableHighlight
+            className="bg-blue-500 rounded-full py-3 mt-2 w-3/4"
+            underlayColor={'#2192FF'}
+            onPress={loginHandler}>
+            <Text className="text-white text-center">
+              {status === 'loading' ? 'Loading...' : 'Sign In'}
+            </Text>
+          </TouchableHighlight>
+        </View>
+      </SafeAreaView>
+    </AlertNotificationRoot>
   );
 }
