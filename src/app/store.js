1334f5397273c620ee30ceb16a3944a32cb41705
danisec <daniaprilyanto35@gmail.com>
2022-11-28 14:20:16 +0700

    update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -3,13 +3,13 @@ import logger from 'redux-logger';
 // import {apiSlice} from '../features/api/apiSlice';
 // import {setupListeners} from '@reduxjs/toolkit/query';
 import authReducer from '../features/auth/authSlice';
-import apiSliceRedux from '../features/api/apiSliceRedux';
+import apiSliceReducer from '../features/api/apiSliceRedux';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     // [apiSlice.reducerPath]: apiSlice.reducer,
-    api: apiSliceRedux,
+    api: apiSliceReducer,
   },
 
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
