5779ea57bf0ab621c6330ef3b875fcfa9cbad9e6
danisec <daniaprilyanto35@gmail.com>
2022-12-02 17:54:46 +0700

    update store cityReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+
 import authReducer from '../features/auth/authSlice';
+import cityReducer from '../features/hotel/citySlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    city: cityReducer,
   },
 
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
