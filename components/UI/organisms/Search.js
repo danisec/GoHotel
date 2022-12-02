b4ac3ecf3ef992d7d34899d7fe42d6ca6856889b
danisec <daniaprilyanto35@gmail.com>
2022-12-02 17:51:17 +0700

    update Search.js

--- components/UI/organisms/Search.js
+++ components/UI/organisms/Search.js
@@ -1,4 +1,8 @@
 import React, {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {useNavigation} from '@react-navigation/native';
+import {fetchCity} from '../../../src/features/hotel/citySlice';
+import {searchHotels} from '../../../src/features/hotel/searchHotelSlice';
 import {
   SafeAreaView,
   View,
@@ -11,11 +15,35 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import DatePicker from 'react-native-date-picker';
 
 export default function Search() {
+  const dispatch = useDispatch();
+  const navigate = useNavigation();
+
+  const getCity = useSelector(state => state.city.cityid);
+
   const [checkin, setCheckin] = useState(new Date());
   const [checkout, setCheckout] = useState(new Date());
 
   const [openCheckin, setOpenCheckin] = useState(false);
   const [openCheckout, setOpenCheckout] = useState(false);
+
+  const [cityId, setCityId] = useState('');
+
+  const formatDateCheckin = () => {
+    const date = new Date(checkin);
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  };
+
+  const formatDateCheckout = () => {
+    const date = new Date(checkout);
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  };
+
+  const handleSearch = () => {
+    dispatch(fetchCity(cityId));
+    dispatch(searchHotels(getCity));
+
+    navigate.navigate('Search Results');
+  };
 
   return (
     <SafeAreaView className="bg-gray-100/30 mx-2 shadow-sm rounded-md shadow-gray-200">
@@ -27,6 +55,7 @@ export default function Search() {
         <TextInput
           placeholder="Where do you want to go?"
           className="py-2 px-10 text-gray-900 text-base bg-white shadow-md shadow-gray-200 rounded-lg"
+          onChangeText={text => setCityId(text)}
         />
 
         <View className="flex flex-row mt-4 justify-between">
@@ -36,12 +65,13 @@ export default function Search() {
               onPress={() => setOpenCheckin(true)}>
               <Icon name="date-range" size={25} color="#A1A1A1" />
               <Text className="text-gray-900 text-base ml-2">
-                {checkin.toDateString()}
+                {formatDateCheckin()}
               </Text>
             </TouchableOpacity>
 
             <DatePicker
               modal
+              mode="date"
               open={openCheckin}
               date={checkin}
               onConfirm={date => {
@@ -60,12 +90,13 @@ export default function Search() {
               onPress={() => setOpenCheckout(true)}>
               <Icon name="date-range" size={25} color="#A1A1A1" />
               <Text className="text-gray-900 text-base ml-2">
-                {checkout.toDateString()}
+                {formatDateCheckout()}
               </Text>
             </TouchableOpacity>
 
             <DatePicker
               modal
+              mode="date"
               open={openCheckout}
               date={checkout}
               onConfirm={date => {
@@ -83,7 +114,8 @@ export default function Search() {
       <View className="my-4 mx-2">
         <TouchableHighlight
           className="p-2 bg-blue-500 rounded-full flex flex-row justify-center py-3"
-          underlayColor={'#2192FF'}>
+          underlayColor={'#2192FF'}
+          onPress={handleSearch}>
           <Text className="text-white text-base font-medium">Search</Text>
         </TouchableHighlight>
       </View>
