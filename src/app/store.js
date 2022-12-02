e5cfdbaa615d99114810e0b4eb3f798270ff14b8
danisec <daniaprilyanto35@gmail.com>
2022-12-02 18:03:01 +0700

    update store detailHotelReducer

--- src/app/store.js
+++ src/app/store.js
@@ -4,12 +4,14 @@ import logger from 'redux-logger';
 import authReducer from '../features/auth/authSlice';
 import cityReducer from '../features/hotel/citySlice';
 import searchHotelReducer from '../features/hotel/searchHotelSlice';
+import detailHotelReducer from '../features/detail-hotel/detailHotelSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     city: cityReducer,
     search: searchHotelReducer,
+    detail: detailHotelReducer,
   },
 
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
